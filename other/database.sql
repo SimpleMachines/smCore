f930ac69e430408767b381fb9b43bb265d24dbb2
Steven Hoffman <git@fustrate.com>
2012-08-09 15:11:00 -0700

    "Official" database dump
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- other/database.sql
+++ other/database.sql
@@ -1,250 +1,224 @@
--- phpMyAdmin SQL Dump
--- version 3.4.9
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Aug 05, 2012 at 07:17 PM
--- Server version: 5.5.19
--- PHP Version: 5.4.0RC4
--- 
--- SQL Dump
--- needs tweaking still
+# ------------------------------------------------------------
+#
+# Replace {db_prefix} with your database prefix.
+#
+# ------------------------------------------------------------
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET NAMES utf8;
 
---
--- Database: `smcore`
---
+# ------------------------------------------------------------
+# Dump of table event_listeners
+# ------------------------------------------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `{db_prefix}event_listeners`;
 
---
--- Table structure for table `acl_privileges`
---
+CREATE TABLE `{db_prefix}event_listeners` (
+  `id_listener` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `listener_name` tinytext CHARACTER SET latin1 NOT NULL,
+  `listener_callback` tinytext CHARACTER SET latin1 NOT NULL,
+  `listener_enabled` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id_listener`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `acl_privileges` (
-  `id_privilege` int(8) unsigned NOT NULL AUTO_INCREMENT,
-  `privilege_module` varchar(255) NOT NULL DEFAULT '',
-  `privilege_name` varchar(255) NOT NULL DEFAULT '',
-  `privilege_group` varchar(255) DEFAULT '',
-  `privilege_user` int(8) unsigned NOT NULL DEFAULT '0',
-  `privilege_allow` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id_privilege`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+# ------------------------------------------------------------
+# Dump of table lang_packages
+# ------------------------------------------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `{db_prefix}lang_packages`;
 
---
--- Table structure for table `acl_roles`
---
-
-CREATE TABLE IF NOT EXISTS `acl_roles` (
-  `id_role` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `role_name` varchar(255) NOT NULL,
-  `role_inherits` varchar(255) NOT NULL,
-  `role_module` smallint(4) unsigned NOT NULL DEFAULT '0',
-  `role_title` varchar(255) NOT NULL DEFAULT '',
-  PRIMARY KEY (`id_role`),
-  UNIQUE KEY `role_name` (`role_name`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
-
---
--- Dumping data for table `acl_roles`
---
-
-INSERT INTO `acl_roles` (`id_role`, `role_name`, `role_inherits`, `role_module`, `role_title`) VALUES
-(1, 'admin', '', 0, 'Administrator'),
-(2, 'member', '', 0, 'Member');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `api_hooks`
---
-
-CREATE TABLE IF NOT EXISTS `api_hooks` (
-  `id_api_hook` int(8) unsigned NOT NULL AUTO_INCREMENT,
-  `api_hook_namespace` varchar(255) NOT NULL DEFAULT '',
-  `api_hook_name` varchar(255) NOT NULL DEFAULT '',
-  `api_hook_callback` varchar(255) NOT NULL DEFAULT '',
-  `api_hook_owner` varchar(255) NOT NULL DEFAULT '',
-  `api_hook_enabled` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id_api_hook`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `api_hooks`
---
-
-INSERT INTO `api_hooks` (`id_api_hook`, `api_hook_namespace`, `api_hook_name`, `api_hook_callback`, `api_hook_owner`, `api_hook_enabled`) VALUES
-(1, 'core', 'user-search', 'Core_API,userSearch', 'com.fustrate.core', 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `hooks`
---
-
-CREATE TABLE IF NOT EXISTS `hooks` (
-  `id_hook` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `hook_namespace` varchar(255) NOT NULL DEFAULT '',
-  `hook_name` varchar(255) NOT NULL DEFAULT '',
-  `hook_callback` varchar(255) NOT NULL DEFAULT '',
-  `hook_owner` varchar(255) NOT NULL DEFAULT '',
-  `hook_enabled` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  PRIMARY KEY (`id_hook`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `languages`
---
-
-CREATE TABLE IF NOT EXISTS `languages` (
-  `id_language` int(11) NOT NULL AUTO_INCREMENT,
-  `language_code` varchar(6) NOT NULL,
-  `language_name` varchar(32) NOT NULL,
-  PRIMARY KEY (`id_language`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `languages`
---
-
-INSERT INTO `languages` (`id_language`, `language_code`, `language_name`) VALUES
-(1, 'en_US', 'english');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `lang_packages`
---
-
-CREATE TABLE IF NOT EXISTS `lang_packages` (
-  `id_package` int(11) NOT NULL AUTO_INCREMENT,
-  `package_name` varchar(32) NOT NULL,
-  `package_type` varchar(32) NOT NULL,
+CREATE TABLE `{db_prefix}lang_packages` (
+  `id_package` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `package_name` varchar(255) NOT NULL DEFAULT '',
+  `package_type` varchar(255) NOT NULL,
   PRIMARY KEY (`id_package`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `lang_packages`
---
+INSERT INTO `{db_prefix}lang_packages` (`id_package`, `package_name`, `package_type`)
+VALUES
+	(1,'org.smcore.common','common'),
+	(2,'org.smcore.auth','common'),
+	(3,'org.smcore.auth.menu','menu');
+# ------------------------------------------------------------
+# Dump of table lang_strings
+# ------------------------------------------------------------
 
-INSERT INTO `lang_packages` (`id_package`, `package_name`, `package_type`) VALUES
-(1, 'org.smcore.common', '');
+DROP TABLE IF EXISTS `{db_prefix}lang_strings`;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `lang_strings`
---
-
-CREATE TABLE IF NOT EXISTS `lang_strings` (
-  `id_string` int(10) unsigned NOT NULL AUTO_INCREMENT,
+CREATE TABLE `{db_prefix}lang_strings` (
   `string_language` smallint(4) unsigned NOT NULL,
   `string_package` smallint(4) unsigned NOT NULL,
   `string_key` varchar(255) CHARACTER SET ascii NOT NULL DEFAULT '',
   `string_value` text NOT NULL,
-  PRIMARY KEY (`id_string`)
+  UNIQUE KEY `language_key` (`string_language`,`string_key`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
--- Dumping data for table `lang_strings`
---
 
-INSERT INTO `lang_strings` (`string_key`, `string_value`, `string_package`, `string_lamguage`) VALUES
-('exceptions.error_code_404', 'HTTP 404 - Page Not Found', 1, 1),
-('exceptions.error_code_403', 'HTTP 403 - Forbidden', 1, 1);
+INSERT INTO `{db_prefix}lang_strings` (`string_language`, `string_package`, `string_key`, `string_value`)
+VALUES
+	(1,2,'auth.exception.register.username_reserved','The username you tried to register with is reserved.'),
+	(1,2,'auth.exceptions.register.email_already_used','The email address you entered is already in use. Did you forget your password?'),
+	(1,2,'auth.exceptions.register.invalid_email','The email address you entered does not seem to be valid.'),
+	(1,2,'auth.exceptions.register.no_email','Please enter an email address.'),
+	(1,2,'auth.exceptions.register.no_password','Please enter a password.'),
+	(1,2,'auth.exceptions.register.password_mismatch','Please make sure you entered the same password in both boxes.'),
+	(1,2,'auth.exceptions.register.username_taken','The username you entered is already taken.'),
+	(1,2,'auth.exceptions.register.username_too_long','The username you entered is too long.'),
+	(1,2,'auth.exceptions.register.username_too_short','The username you entered is too short.'),
+	(1,2,'auth.login.log_in','Log In'),
+	(1,3,'auth.menu.login','Log In'),
+	(1,3,'auth.menu.register','Register'),
+	(1,2,'auth.register','Register'),
+	(1,2,'auth.register.agree','I accept the terms of the agreement and I am at least 13 years old.'),
+	(1,2,'auth.register.agreement','Registration Agreement'),
+	(1,2,'auth.register.agreement_text','This is the registration agreement text.\n\nYou should agree to it.'),
+	(1,2,'auth.register.agree_young','I accept the terms of the agreement and I am younger than 13 years old.'),
+	(1,2,'auth.register.captcha','Captcha'),
+	(1,2,'auth.register.finished','Finished Registering'),
+	(1,2,'auth.register.form','Registration Form'),
+	(1,2,'auth.register.form_title','Registration Form'),
+	(1,2,'auth.register.required_info','Required Information'),
+	(1,2,'auth.register.verification','Verification'),
+	(1,2,'auth.register.verify_password','Verify Password'),
+	(1,1,'email','Email'),
+	(1,1,'exceptions.api.invalid_route','Invalid API route.'),
+	(1,1,'exceptions.error_code_403','You are not allowed to access this page.'),
+	(1,1,'exceptions.error_code_404','Page not found.'),
+	(1,1,'exceptions.error_code_unknown','Unknown error.'),
+	(1,1,'exceptions.modules.invalid_controller','Invalid controller \"%s\", file not found.'),
+	(1,1,'exceptions.modules.method_not_callable','Module controller method not callable, found controller \"%1$s\" but it does not have a method named \"%2$s\".'),
+	(1,1,'password','Password'),
+	(1,1,'save','Save'),
+	(1,1,'submit','Submit'),
+	(1,1,'username','Username\n');
 
--- --------------------------------------------------------
+# ------------------------------------------------------------
+# Dump of table languages
+# ------------------------------------------------------------
 
---
--- Table structure for table `modules`
---
+DROP TABLE IF EXISTS `{db_prefix}languages`;
 
-CREATE TABLE IF NOT EXISTS `modules` (
-  `id_module` smallint(4) NOT NULL AUTO_INCREMENT,
-  `module_name` varchar(255) NOT NULL DEFAULT '',
-  `module_url` varchar(255) NOT NULL,
-  `module_identifier` varchar(255) NOT NULL,
-  `module_class` varchar(255) NOT NULL,
-  PRIMARY KEY (`id_module`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+CREATE TABLE `{db_prefix}languages` (
+  `id_language` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
+  `language_code` varchar(255) NOT NULL DEFAULT '',
+  `language_name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_language`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+INSERT INTO `{db_prefix}languages` (`id_language`, `language_code`, `language_name`)
+VALUES
+	(1,'en_US','English (American)');
 
---
--- Table structure for table `routes`
---
+# ------------------------------------------------------------
+# Dump of table menu
+# ------------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `routes` (
-  `id_route` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
-  `route_url` varchar(255) NOT NULL,
-  `route_module` smallint(4) unsigned NOT NULL,
-  `route_parent` mediumint(8) unsigned NOT NULL DEFAULT '0',
-  `route_title` varchar(255) NOT NULL,
-  `route_method` varchar(255) NOT NULL DEFAULT '',
-  `route_default` tinyint(1) unsigned NOT NULL DEFAULT '0',
-  `route_visible` tinyint(1) unsigned NOT NULL DEFAULT '1',
-  `route_allowed_roles` varchar(255) NOT NULL DEFAULT '',
-  `route_order` smallint(2) unsigned NOT NULL DEFAULT '99',
-  PRIMARY KEY (`id_route`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+DROP TABLE IF EXISTS `{db_prefix}menu`;
 
--- --------------------------------------------------------
+CREATE TABLE `{db_prefix}menu` (
+  `id_menu` int(8) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_title` varchar(255) NOT NULL DEFAULT '',
+  `menu_visible` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `menu_module` varchar(255) NOT NULL DEFAULT '',
+  `menu_parent` int(8) unsigned NOT NULL,
+  `menu_order` tinyint(3) unsigned NOT NULL DEFAULT '50',
+  `menu_url` varchar(255) NOT NULL,
+  `menu_permission` varchar(255) NOT NULL DEFAULT '',
+  `menu_name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_menu`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Table structure for table `sessions`
---
+INSERT INTO `{db_prefix}menu` (`id_menu`, `menu_title`, `menu_visible`, `menu_module`, `menu_parent`, `menu_order`, `menu_url`, `menu_permission`, `menu_name`)
+VALUES
+	(1,'auth.menu.login',1,'org.smcore.auth',0,98,'/login/','org.smcore.auth.is_guest','login'),
+	(2,'auth.menu.register',1,'org.smcore.auth',0,99,'/register/','org.smcore.auth.is_guest','register'),
+	(3,'auth.menu.logout',1,'org.smcore.auth',0,99,'/logout/','org.smcore.auth.is_member','logout');
 
-CREATE TABLE IF NOT EXISTS `sessions` (
-  `id_session` char(32) NOT NULL,
-  `session_expires` int(10) unsigned NOT NULL,
+# ------------------------------------------------------------
+# Dump of table permissions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `{db_prefix}permissions`;
+
+CREATE TABLE `{db_prefix}permissions` (
+  `id_permission` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `permission_role` mediumint(8) unsigned NOT NULL,
+  `permission_namespace` varchar(255) NOT NULL DEFAULT '',
+  `permission_state` tinyint(1) NOT NULL DEFAULT '0',
+  `permission_name` varchar(255) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id_permission`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+# ------------------------------------------------------------
+# Dump of table roles
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `{db_prefix}roles`;
+
+CREATE TABLE `{db_prefix}roles` (
+  `id_role` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
+  `role_title` varchar(255) NOT NULL DEFAULT '',
+  `role_inherits` mediumint(8) unsigned NOT NULL,
+  `role_permission` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_role`),
+  UNIQUE KEY `role_name` (`role_title`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+INSERT INTO `{db_prefix}roles` (`id_role`, `role_title`, `role_inherits`, `role_permission`)
+VALUES
+	(0,'Guest',0,'is_guest'),
+	(1,'Administrator',2,'is_admin'),
+	(2,'Member',0,'is_member');
+
+# ------------------------------------------------------------
+# Dump of table sessions
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `{db_prefix}sessions`;
+
+CREATE TABLE `{db_prefix}sessions` (
+  `id_session` char(32) NOT NULL DEFAULT '',
+  `session_expires` int(10) NOT NULL,
   `session_data` text NOT NULL,
-  PRIMARY KEY (`session_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id_session`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- --------------------------------------------------------
+# ------------------------------------------------------------
+# Dump of table themes
+# ------------------------------------------------------------
 
---
--- Table structure for table `themes`
---
+DROP TABLE IF EXISTS `{db_prefix}themes`;
 
-CREATE TABLE IF NOT EXISTS `themes` (
+CREATE TABLE `{db_prefix}themes` (
   `id_theme` smallint(4) unsigned NOT NULL AUTO_INCREMENT,
   `theme_name` varchar(255) NOT NULL,
   `theme_dir` varchar(255) NOT NULL DEFAULT '',
-  `theme_class` varchar(255) NOT NULL DEFAULT '',
   PRIMARY KEY (`id_theme`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `themes`
---
+INSERT INTO `{db_prefix}themes` (`id_theme`, `theme_name`, `theme_dir`)
+VALUES
+	(1,'Default Theme','default');
 
-INSERT INTO `themes` (`id_theme`, `theme_name`, `theme_dir`, `theme_class`) VALUES
-(1, 'Default', 'default', 'DefaultTheme');
+# ------------------------------------------------------------
+# Dump of table users
+# ------------------------------------------------------------
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `{db_prefix}users`;
 
---
--- Table structure for table `users`
---
-
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `{db_prefix}users` (
   `id_user` int(8) unsigned NOT NULL AUTO_INCREMENT,
   `user_login` varchar(45) NOT NULL,
-  `user_pass` varchar(64) NOT NULL,
-  `user_primary_role` smallint(4) unsigned NOT NULL DEFAULT '0',
-  `user_display_name` varchar(50) NOT NULL,
-  `user_given_name` varchar(50) NOT NULL,
   `user_email` varchar(255) NOT NULL,
-  `user_registered` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_registered` int(10) unsigned NOT NULL,
   `user_token` varchar(32) NOT NULL,
-  `user_active` tinyint(1) unsigned NOT NULL DEFAULT '1',
+  `user_active` tinyint(1) NOT NULL DEFAULT '1',
+  `user_pass` varchar(64) NOT NULL DEFAULT '',
+  `user_primary_role` smallint(4) unsigned NOT NULL DEFAULT '2',
+  `user_display_name` varchar(50) NOT NULL DEFAULT '',
   `user_additional_roles` varchar(255) NOT NULL DEFAULT '',
-  `user_language` varchar(255) NOT NULL,
+  `user_language` smallint(4) unsigned NOT NULL DEFAULT '1',
+  `user_theme` smallint(4) unsigned NOT NULL DEFAULT '1',
+  `user_activation` varchar(32) NOT NULL DEFAULT '',
+  `user_reset_key` varchar(32) NOT NULL DEFAULT '',
   UNIQUE KEY `id_user` (`id_user`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
