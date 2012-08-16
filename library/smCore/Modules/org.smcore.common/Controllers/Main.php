b3533f53a23c53bd57b6dff3c78afc6d4a161ff6
Steven Hoffman <git@fustrate.com>
2012-08-16 15:03:36 -0700

    Credits page, terms of use & privacy policy page
    Signed-off-by: Steven Hoffman <git@fustrate.com>

---
+++ library/smCore/Modules/org.smcore.common/Controllers/Main.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * smCore Common Module - Main Controller
+ *
+ * @package smCore
+ * @author smCore Dev Team
+ * @license MPL 1.1
+ * @version 1.0 Alpha
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this package except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * The Original Code is smCore.
+ *
+ * The Initial Developer of the Original Code is the smCore project.
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2011
+ * the Initial Developer. All Rights Reserved.
+ */
+
+namespace smCore\Modules\Common\Controllers;
+
+use smCore\Application, smCore\Module\Controller, smCore\Storage;
+
+class Main extends Controller
+{
+	public function preDispatch()
+	{
+		$this->_getParentModule()->loadLangPackage();
+	}
+
+	public function credits()
+	{
+		$module = $this->_getParentModule();
+
+		return $module->render('credits');
+	}
+
+	public function terms_and_privacy()
+	{
+		$module = $this->_getParentModule();
+
+		return $module->render('terms_and_privacy');
+	}
+}
