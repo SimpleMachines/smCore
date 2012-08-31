7d330235bcc0306f69e9eac91e7d4f2ca0bea02f
Steven Hoffman <git@fustrate.com>
2012-08-31 11:22:00 -0700

    + Lang strings and user administration menu items
    Signed-off-by: Steven Hoffman <git@fustrate.com>

--- library/smCore/Modules/org.smcore.admin/Events.php
+++ library/smCore/Modules/org.smcore.admin/Events.php
@@ -12,18 +12,20 @@ class Events
 
 	public static function menu(Event $event)
 	{
+		$lang = $event['app']['lang'];
+
 		if ($event['app']['user']->hasPermission('org.smcore.auth.is_admin'))
 		{
-			$admin = new MenuItem('admin', 'Admin', '/admin/', true, 90);
+			$admin = new MenuItem('admin', $lang->get('admin.menu.admin'), '/admin/', true, 90);
 
-			$main        = new MenuItem('admin_center', 'Main', '/admin/', true, 1);
-			$config      = new MenuItem('admin_config', 'Config', '/admin/config/', true, 10);
-			$modules     = new MenuItem('admin_modules', 'Modules', '/admin/modules/', true, 20);
-			$maintenance = new MenuItem('admin_maintenance', 'Maintenance', '/admin/maintenance/', true, 90);
+			$main        = new MenuItem('admin_center', $lang->get('admin.menu.main'), '/admin/', true, 1);
+			$config      = new MenuItem('admin_config', $lang->get('admin.menu.config'), '/admin/config/', true, 10);
+			$modules     = new MenuItem('admin_modules', $lang->get('admin.menu.modules'), '/admin/modules/', true, 20);
+			$maintenance = new MenuItem('admin_maintenance', $lang->get('admin.menu.maintenance'), '/admin/maintenance/', true, 90);
 
-			$maintenance->addItem(new MenuItem('admin_maintenance_main', 'General', '/admin/maintenance/', true, 1));
-			$maintenance->addItem(new MenuItem('admin_maintenance_database', 'Database', '/admin/maintenance/database/', true, 10));
-			$maintenance->addItem(new MenuItem('admin_maintenance_cache', 'Cache', '/admin/maintenance/cache/', true, 20));
+			$maintenance->addItem(new MenuItem('admin_maintenance_main', $lang->get('admin.menu.maintenance.main'), '/admin/maintenance/', true, 1));
+			$maintenance->addItem(new MenuItem('admin_maintenance_database', $lang->get('admin.menu.maintenance.database'), '/admin/maintenance/database/', true, 10));
+			$maintenance->addItem(new MenuItem('admin_maintenance_cache', $lang->get('admin.menu.maintenance.cache'), '/admin/maintenance/cache/', true, 20));
 
 			$admin->addItem($main);
 			$admin->addItem($config);
--- library/smCore/Modules/org.smcore.auth/Events.php
+++ library/smCore/Modules/org.smcore.auth/Events.php
@@ -12,21 +12,30 @@ class Events
 
 	public static function menu(Event $event)
 	{
+		$lang = $event['app']['lang'];
+
 		if ($event['app']['user']->hasPermission('org.smcore.auth.is_guest'))
 		{
-			$event['menu']->addItem(new MenuItem('login', 'Log In', '/login/', true, 98));
-			$event['menu']->addItem(new MenuItem('register', 'Register', '/register/', true, 99));
+			$event['menu']->addItem(new MenuItem('login', $lang->get('auth.menu.login'), '/login/', true, 98));
+			$event['menu']->addItem(new MenuItem('register', $lang->get('auth.menu.register'), '/register/', true, 99));
 		}
 		else
 		{
-			$event['menu']->addItem(new MenuItem('logout', 'Log Out', '/logout/', true, 99));
+			$event['menu']->addItem(new MenuItem('logout', $lang->get('auth.menu.logout'), '/logout/', true, 99));
 
-			$profile = new MenuItem('profile', 'Profile', '/profile/', true, 80);
+			$profile = new MenuItem('profile', $lang->get('auth.menu.profile'), '/profile/', true, 80);
 
-			$profile->addItem(new MenuItem('profile_main', 'My Account', '/profile/', true, 1));
-			$profile->addItem(new MenuItem('profile_settings', 'Settings', '/profile/settings/', true, 50));
+			$profile->addItem(new MenuItem('profile_main', $lang->get('auth.menu.profile.main'), '/profile/', true, 1));
+			$profile->addItem(new MenuItem('profile_settings', $lang->get('auth.menu.profile.settings'), '/profile/settings/', true, 50));
 
 			$event['menu']->addItem($profile);
+		}
+
+		// The admin menu item will only exist if the current user is an admin
+		if (isset($event['menu']['admin']))
+		{
+			$event['menu']['admin']->addItem(new MenuItem('admin_users', $lang->get('auth.menu.admin.users'), '/admin/users/', true, 30));
+			$event['menu']['admin']['admin_maintenance']->addItem(new MenuItem('admin_maintenance_users', $lang->get('auth.menu.admin.maintenance.users'), '/admin/maintenance/users/', true, 30));
 		}
 	}
 }
